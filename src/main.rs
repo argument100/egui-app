5d24b4905eea20ec71740ceea03dd656f42fa683
argument100 <100blendy@gmail.com>
2024-03-26 22:29:03 +0900

    Section 5 completed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let native_option = eframe::NativeOptions { default_theme: eframe::Theme::Light, ..Default::default() };
+    let _ = eframe::run_native("My egui App", native_option, Box::new(|cc| Box::new(MyEguiApp::new(cc))));
+}
+
+#[derive(Default)]
+struct MyEguiApp {}
+
+impl MyEguiApp {
+    fn new(_cc: &eframe::CreationContext) -> Self {
+        Self::default()
+    }
+}
+
+impl eframe::App for MyEguiApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello World!");
+            let label_text = egui::RichText::new("This is sample message.")
+                .size(32.0)
+                .color(egui::Color32::from_rgba_premultiplied(255, 0, 0, 100))
+                .italics();
+            let label = egui::Label::new(label_text);
+            ui.add(label)
+        });
+    }
 }
