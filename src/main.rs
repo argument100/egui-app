f15b0ce49cc015a9aa2734f9d8ebaf7c9777958c
argument100 <100blendy@gmail.com>
2024-03-27 22:14:43 +0900

    add button

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 fn main() {
-    let native_option = eframe::NativeOptions { default_theme: eframe::Theme::Light, ..Default::default() };
+    let native_option = eframe::NativeOptions {
+        default_theme: eframe::Theme::Light,
+        initial_window_size: Some(egui::Vec2 {x:400.0, y:200.0}),
+        ..Default::default()
+    };
     let _ = eframe::run_native("My egui App", native_option, Box::new(|cc| Box::new(MyEguiApp::new(cc))));
 }
 
-#[derive(Default)]
-struct MyEguiApp {}
+struct MyEguiApp {
+    pub value:usize,
+}
+
+impl Default for MyEguiApp {
+    fn default() -> MyEguiApp {
+        MyEguiApp {
+            value: 0,
+        }
+    }
+}
 
 impl MyEguiApp {
     fn new(_cc: &eframe::CreationContext) -> Self {
@@ -16,12 +29,22 @@ impl eframe::App for MyEguiApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Hello World!");
-            let label_text = egui::RichText::new("This is sample message.")
-                .size(32.0)
-                .color(egui::Color32::from_rgba_premultiplied(255, 0, 0, 100))
-                .italics();
+
+            ui.spacing();
+
+            let msg = format!("click {} times.", self.value);
+            let label_text = egui::RichText::new(msg).size(32.0);
             let label = egui::Label::new(label_text);
-            ui.add(label)
+            ui.add(label);
+
+            ui.separator();
+
+            let btn_txt = egui::RichText::new("Click!").font(egui::FontId::proportional(24.0));
+            let btn = egui::Button::new(btn_txt);
+            let resp = ui.add_sized(egui::Vec2 {x:150.0, y:40.0}, btn);
+            if resp.clicked() {
+                self.value += 1;
+            }
         });
     }
 }
